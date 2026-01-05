2941ff3d6a504e969e8b57930d09c29fd8dc1b9f
우형래 <174302871+hyung-rae@users.noreply.github.com>
2026-01-05 17:16:06 +0900

    ♻️ Refactor: SettingDialog 컴포넌트 코드 정리

--- src/components/setting-dialog/_component.tsx
+++ src/components/setting-dialog/_component.tsx
@@ -3,7 +3,7 @@ import type { ContentType, CustomLayoutItem } from "@/types"
 import { createContentId, getInitSubType } from "@/utils"
 import BallotIcon from "@mui/icons-material/Ballot"
 import PieChartIcon from "@mui/icons-material/PieChart"
-import StackedBarChartIcon from "@mui/icons-material/StackedBarChart"
+import SsidChartIcon from "@mui/icons-material/SsidChart"
 import TableChartIcon from "@mui/icons-material/TableChart"
 import {
   Button,
@@ -22,7 +22,7 @@ import {
   type SelectChangeEvent,
 } from "@mui/material"
 import { useQuery } from "@tanstack/react-query"
-import { useMemo, useState } from "react"
+import { useMemo, useState, type JSX } from "react"
 import Select from "../select"
 
 type SettingDialogProps = {
@@ -31,11 +31,7 @@ type SettingDialogProps = {
   handleAddContent: (content: CustomLayoutItem) => void
 }
 
-const SettingDialog = ({
-  open,
-  close,
-  handleAddContent,
-}: SettingDialogProps) => {
+const SettingDialog = ({ open, close, handleAddContent }: SettingDialogProps) => {
   const [type, setType] = useState<ContentType>("feed")
   const [data, setData] = useState<string>()
 
@@ -86,35 +82,18 @@ const SettingDialog = ({
         <Stack direction="column" gap={2} sx={{ mt: 2, mb: 4 }}>
           <Typography variant="body1">통계 유형</Typography>
 
-          <CustomToggleButtonGroup
-            color="primary"
-            value={type}
-            exclusive
-            onChange={handleType}
-          >
-            <Tooltip followCursor placement="top" title="line, bar, column">
-              <ToggleButton value="series">
-                <StackedBarChartIcon />
-              </ToggleButton>
-            </Tooltip>
+          <CustomToggleButtonGroup exclusive color="primary" value={type} onChange={handleType}>
+            <ContentButton title="line, bar, column" value="series" icon={<SsidChartIcon />} />
 
-            <Tooltip followCursor placement="top" title="pie, donut, circle">
-              <ToggleButton value="distribution">
-                <PieChartIcon />
-              </ToggleButton>
-            </Tooltip>
+            <ContentButton
+              title="pie, donut, circle"
+              value="distribution"
+              icon={<PieChartIcon />}
+            />
 
-            <Tooltip followCursor placement="top" title="table">
-              <ToggleButton value="table">
-                <TableChartIcon />
-              </ToggleButton>
-            </Tooltip>
+            <ContentButton title="table" value="table" icon={<TableChartIcon />} />
 
-            <Tooltip followCursor placement="top" title="feed">
-              <ToggleButton value="feed">
-                <BallotIcon />
-              </ToggleButton>
-            </Tooltip>
+            <ContentButton title="feed" value="feed" icon={<BallotIcon />} />
           </CustomToggleButtonGroup>
         </Stack>
 
@@ -140,23 +119,35 @@ const SettingDialog = ({
 
 export default SettingDialog
 
+const ContentButton = ({
+  title,
+  value,
+  icon,
+}: {
+  title: string
+  value: ContentType
+  icon: JSX.Element
+}) => {
+  return (
+    <Tooltip followCursor placement="top" title={title}>
+      <ToggleButton value={value}>{icon}</ToggleButton>
+    </Tooltip>
+  )
+}
+
 const CustomToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
   justifyContent: "space-around",
-  [`& .${toggleButtonGroupClasses.firstButton}, & .${toggleButtonGroupClasses.middleButton}`]:
-    {
-      borderTopRightRadius: (theme.vars || theme).shape.borderRadius,
-      borderBottomRightRadius: (theme.vars || theme).shape.borderRadius,
-    },
-  [`& .${toggleButtonGroupClasses.lastButton}, & .${toggleButtonGroupClasses.middleButton}`]:
-    {
-      borderTopLeftRadius: (theme.vars || theme).shape.borderRadius,
-      borderBottomLeftRadius: (theme.vars || theme).shape.borderRadius,
-      borderLeft: `1px solid ${(theme.vars || theme).palette.divider}`,
-    },
+  [`& .${toggleButtonGroupClasses.firstButton}, & .${toggleButtonGroupClasses.middleButton}`]: {
+    borderTopRightRadius: (theme.vars || theme).shape.borderRadius,
+    borderBottomRightRadius: (theme.vars || theme).shape.borderRadius,
+  },
+  [`& .${toggleButtonGroupClasses.lastButton}, & .${toggleButtonGroupClasses.middleButton}`]: {
+    borderTopLeftRadius: (theme.vars || theme).shape.borderRadius,
+    borderBottomLeftRadius: (theme.vars || theme).shape.borderRadius,
+    borderLeft: `1px solid ${(theme.vars || theme).palette.divider}`,
+  },
   [`& .${toggleButtonGroupClasses.lastButton}.${toggleButtonClasses.disabled}, & .${toggleButtonGroupClasses.middleButton}.${toggleButtonClasses.disabled}`]:
     {
-      borderLeft: `1px solid ${
-        (theme.vars || theme).palette.action.disabledBackground
-      }`,
+      borderLeft: `1px solid ${(theme.vars || theme).palette.action.disabledBackground}`,
     },
 }))
